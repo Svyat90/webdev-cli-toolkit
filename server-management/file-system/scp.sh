8b4d0f1f7f5f9e8f2697d9cc68240cb2f72fea11
svyat90 <matrix.yurinets@gmail.com>
2024-06-18 13:36:05 -0500

    scp

--- server-management/file-system/scp.sh
+++ server-management/file-system/scp.sh
@@ -1,8 +1,2 @@
-
-scp -r /dir root@host:/path             # copy dorectory to remote server recursively
-
-tar -czf archive.tar.gz ./              # archive directory using TAR tool
-tar -xf archive.tar.gz                  # extract using TAR tool
-
-zip -r archive.zip /dir                 # archive directory using ZIP tool
-unzip ~/archive.zip -d ~/dir            # extract using ZIP tool
+# copy directory to remote server recursively
+scp -r /dir root@host:/path
