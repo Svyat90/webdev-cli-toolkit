4d55850163d4b4fc50c85f3365c184140959223b
svyat90 <matrix.yurinets@gmail.com>
2024-07-12 16:30:20 -0500

    clear space

--- server-management/file-system/clear-space.sh
+++ server-management/file-system/clear-space.sh
@@ -1,12 +1,21 @@
-# find the top 10 largest files on the entire system
-sudo find / -type f -exec du -h {} + | sort -rh | head -n 10
-
-# find top 10 largest files in specific directory
-sudo du -ah /home | sort -rh | head -n 10
-
 # Cleaning packages
 sudo apt autoremove
 sudo apt autoclean
 
-# Remove the oldest archived journal files until the disk space they use falls below the specified size
-sudo journalctl --vacuum-size 10M
+# Clean Up Systemd Journal Logs
+sudo journalctl --vacuum-time=3d  # Keeps logs for the last 3 days
+sudo journalctl --vacuum-size=100M  # Limits the log size to 100MB
+
+# Remove Temporary Files
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
+
+# Check Disk Usage
+du -h --max-depth=1 /  # Check the size of directories in the root
+du -h --max-depth=1 ~/  # Check the size of directories in the home directory
+
+# Find the top 10 largest files on the entire system
+sudo find / -type f -exec du -h {} + | sort -rh | head -n 10
+
+# Find top 10 largest files in specific directory
+sudo du -ah /home | sort -rh | head -n 10
